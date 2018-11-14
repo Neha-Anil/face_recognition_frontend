1a611843f78d62d581fd18c21e7e14cdeb8ae3cc
Neha A <42008366+Neha-Anil@users.noreply.github.com>
2018-11-14 21:55:56 +0530

    deploy

--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -58,19 +58,19 @@ class Register extends React.Component{
 	    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 	      <legend className="f1 fw6 ph0 mh0">Register</legend>
 	      <div className="mt3">
-	        <label className="db fw6 lh-copy f4" for="name">Name</label>
+	        <label className="db fw6 lh-copy f4" htmlFor="name">Name</label>
 	        <input 
 	        onChange={this.onNameChange}
 	        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name"/>
 	      </div>
 	      <div className="mt3">
-	        <label className="db fw6 lh-copy f4" for="email-address">Email</label>
+	        <label className="db fw6 lh-copy f4" htmlFor="email-address">Email</label>
 	        <input 
 	        onChange={this.onEmailChange}
 	        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
 	      </div>
 	      <div className="mv3">
-	        <label className="db fw6 lh-copy f4" for="password">Password</label>
+	        <label className="db fw6 lh-copy f4" htmlFor="password">Password</label>
 	        <div className="db fw6 lh-copy f6"><center>(of length 8 including a number and special character)</center></div> 
 	        <input 
 	        onChange={this.onPasswordChange}
